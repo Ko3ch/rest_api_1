3548678719c8f3a7835f5d4ae22c39bad780319d
Ko3ch <tomko3ch@gmail.com>
2022-06-15 21:26:33 +0300

    saved changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,43 @@
-from django.http import JsonResponse
-import json
+# from django.http import JsonResponse
+# import json
 from products.models import Product
+from products.serializers import ProductSerializer
 from django.forms.models import model_to_dict
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
+
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by('?').first()
+
+    """
+    DRF API view
+    """
+    # if request.method != "POST":
+    #     return Response({"detail": "GET not allowed"})
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-    if model_data:
-        # before model_to_dict
-        # data['id'] = model_data.id
-        # data['title'] = model_data.title
-        # data['content'] = model_data.content
-        # data['price'] = model_data.price
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
+# def api_home(request, *args, **kwargs):
+#     instance = Product.objects.all().order_by('?').first()
+#     data = {}
+#     if instance:
+#         # before model_to_dict
+#         # data['id'] = instance.id
+#         # data['title'] = instance.title
+#         # data['content'] = instance.content
+#         # data['price'] = instance.price
 
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+#         data = model_to_dict(instance, fields=['id', 'title', 'price'])
 
-        # model instance (model data)
-        # turn it python dictionary
-        # return JSON on client
-    return JsonResponse(data)
+#         # model instance (model data)
+#         # turn it python dictionary
+#         # return JSON on client
+#     return JsonResponse(data)
 
